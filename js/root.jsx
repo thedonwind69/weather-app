571ea24d051b0913c61e115efbdc858de6c6589d
Don Nguyen <thedonwind69@gmail.com>
2019-05-08 18:51:49 -0700

    add

--- js/root.jsx
+++ js/root.jsx
@@ -11,7 +11,7 @@ class Root extends React.Component {
     render () {
         return (
             <div>
-                <h1>WEATHER APP BITCH</h1>
+                <h1>Here's the weather!</h1>
                 <Weather />
             </div>
         )
